230b459196e3468bfef9d421a5ae6a017b1e634a
KeyTey <k.terada.1211@gmail.com>
2020-03-25 03:42:35 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kenta Terada on 2020/03/25.
+//  Copyright © 2020 Kenta Terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
