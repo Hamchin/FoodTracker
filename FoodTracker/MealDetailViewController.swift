ce9a15be7607fbc3e727c7ce75aa2238c240b22a
KeyTey <k.terada.1211@gmail.com>
2020-04-06 04:34:10 +0900

    Update

---
+++ FoodTracker/MealDetailViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kenta Terada on 2020/03/25.
+//  Copyright © 2020 Kenta Terada. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class MealDetailViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    /*
+    This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+    or constructed as part of adding a new meal.
+    */
+    var meal: Meal?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
+    }
+
+    // MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owingNavigationController = navigationController {
+            owingNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealDetailViewController is not inside a navigation controller.")
+        }
+    }
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: OSLogType.debug)
+            return
+        }
+        let name: String = nameTextField.text ?? ""
+        let photo: UIImage? = photoImageView.image
+        let rating: Int = ratingControl.rating
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+
+    // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
+// MARK: UITextFieldDelegate
+extension MealDetailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        navigationItem.title = textField.text
+    }
+    // MARK: Private Methods
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text: String = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
+}
+
+// MARK: UIImagePickerControllerDelegate+UINavigationControllerDelegate
+extension MealDetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // Set photoImageView to display the selectedImage
+        photoImageView.image = self.cropImage(image: selectedImage, w: 320, h: 320)
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    func cropImage(image: UIImage, w: Int, h: Int) -> UIImage {
+        let origRef = image.cgImage
+        let origWidth = Int(origRef!.width)
+        let origHeight = Int(origRef!.height)
+        var resizeWidth: Int = 0
+        var resizeHeight: Int = 0
+        if (origWidth < origHeight) {
+            resizeWidth = w
+            resizeHeight = origHeight * resizeWidth / origWidth
+        }
+        else {
+            resizeHeight = h
+            resizeWidth = origWidth * resizeHeight / origHeight
+        }
+        let resizeSize = CGSize.init(width: CGFloat(resizeWidth), height: CGFloat(resizeHeight))
+        UIGraphicsBeginImageContext(resizeSize)
+        image.draw(in: CGRect.init(x: 0, y: 0, width: CGFloat(resizeWidth), height: CGFloat(resizeHeight)))
+        let resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        let cropRect = CGRect.init(x: CGFloat((resizeWidth - w) / 2), y: CGFloat((resizeHeight - h) / 2), width: CGFloat(w), height: CGFloat(h))
+        let cropRef = resizeImage!.cgImage!.cropping(to: cropRect)
+        let cropImage = UIImage(cgImage: cropRef!)
+        return cropImage
+    }
+}
---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kenta Terada on 2020/03/29.
+//  Copyright © 2020 Kenta Terada. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
